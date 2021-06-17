d01e56570f7dec308994994008614528ef6542bc
Ankit <goyalankit021@gmail.com>
2021-06-17 11:50:48 +0530

    Learning merge conflict

--- Code/C++Code/helloWorld.c
+++ Code/C++Code/helloWorld.c
@@ -1,1 +1,12 @@
-include <Stdio.h>
+#include <Stdio.h>
+
+void greeting(char []name);
+
+int main(){
+	char[] name="Aliaa Bhat"
+		greeting(name);
+}
+
+void greeting(char[] name){
+	printf("Hey, Where were you? %s",name);
+}
--- Code/JavaCode/simple.java
+++ Code/JavaCode/simple.java
@@ -1,1 +1,17 @@
 import java.util.*;
+
+public class Main{
+	public static void main(String[] args){
+	helloWorld();
+	}
+
+	public static void helloWorld(){
+		System.out.println("Hello");
+		completeThis();
+	}
+
+	public static void completeThis(){
+		System.out.println("World");
+	}
+}
+
